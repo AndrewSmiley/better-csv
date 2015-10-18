be1aaf18163ed68d11228c63a32c9221cf850a41
Andrew Smiley <andrew.smiley@live.com>
2015-10-18 11:10:13 -0400

    Beginning to fix codebase

---
+++ excel_functions.py
@@ -0,0 +1,12 @@
+__author__ = 'pridemai'
+import sys
+sys.path.insert(0, './better-csv')
+from parse_functions  import BetterCSV
+
+def update_row(master_row, data_row, master_row_columns, data_row_columns):
+    pass
+
+
+def iterate(master_copy,data_copy, master_search_columns, data_search_columns):
+    
+

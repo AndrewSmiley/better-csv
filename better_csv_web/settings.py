5a0c9d3812ddc18d0606e7645164871912a7bb0c
Andrew Smiley <andrew.smiley@live.com>
2015-10-18 23:07:06 -0400

    Adding more data

--- better_csv_web/settings.py
+++ better_csv_web/settings.py
@@ -1,11 +1,11 @@
 """
-Django settings for better_csv_web project.
+Django settings for generic_django_project project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/dev/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,10 +14,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_a%nq3zo3#@6h=k9z+h!xvf_^f%oex^gh=t&91l0*&o=76708i'
+SECRET_KEY = 'x!k2=s$8)iu2-sjob5=cxf3ax59)#6-czbkrs4ch!ot#we#!9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -47,23 +47,49 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'better_csv_web.urls'
+ROOT_URLCONF = 'generic_django_project.urls'
 
-WSGI_APPLICATION = 'better_csv_web.wsgi.application'
+WSGI_APPLICATION = 'generic_django_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'storage.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
+STATIC_ROOT=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
+
+STATIC_URL = '/static/'
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+
+#     'django.template.loaders.eggs.Loader',
+)
+TEMPLATE_DIRS = (
+    STATIC_ROOT + '/templates/',
+)
+MEDIA_ROOT = BASE_DIR + '/media/'
+
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/dev/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -77,6 +103,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
--- tester.py
+++ tester.py
@@ -40,30 +40,61 @@ leap_tracker3= better_csv.get_lists(better_csv.get_lines(open("leap_tracker3.csv
 mountain_top2= better_csv.get_lists(better_csv.get_lines(open("mountain_top2.csv").read()))
 raw_qpe= better_csv.get_lists(better_csv.get_lines(open("raw_qpe.csv").read()))
 ge_lta2= better_csv.get_lists(better_csv.get_lines(open("ge_lta2.csv___jb_bak___").read()))
-print "files loaded"
 
-master_copy = iterate(master_copy, firth_rixon, [1,10],[16,17],{18:12, 16:15,19:13,31:11},"Firth Rixson")
-master_copy = iterate(master_copy,welded_ring,[1,10],[1,2],{18:8,16:6,19:9,7:5}, "Welded Ring 1")
-# master_copy = iterate(master_copy, raw_qpe,[1,10],[0],{4:6},"Raw QPE")
-master_copy = iterate(master_copy,sandy, [1,10],[1,8],{7:6,11:9,14:12}, "Sandy's Data")
-master_copy = iterate(master_copy,mountain_top, [1,10],[0], {18:3,16:5,7:1}, "Mountain Top")
-master_copy = iterate(master_copy,cfw,[1,10],[0], {7:1, 18:2, 19:3, 15:4, 16:5, 14:7}, "CFW 1")
-master_copy = iterate(master_copy,suzhou,[1,10],[0,6], {7:4,11:7,14:10}, "Suzhou 1")
-master_copy = iterate(master_copy,leap_tracker,[1,10],[1,3], {4:2,31:8,19:9,7:30}, "LEAP tracker 1")
-master_copy = iterate(master_copy, leap_tracker_welded_ring_update,[1,10],[1,2],{16:17,19:8,31:7,7:9,14:25}, "LEAP welded ring update")
-master_copy = iterate(master_copy,tei,[1,10],[1,8],{7:6,11:9,14:12}, "TEI")
-master_copy = iterate(master_copy,cfw_lta,[1,10],[0], {7:1,18:2,19:3,16:5,14:7}, "CFW LTA")
-master_copy = iterate(master_copy, firth_rixson2, [1,10],[15,16,17,18], {31:10,18:11,19:12,15:13,7:14}, "Firth Rixson 2")
-master_copy = iterate(master_copy,frisa_lta,[1,10],[2,3], {7:6,18:9,19:11}, "Frisa LTA")
-master_copy = iterate(master_copy,ge_lta, [1,10],[16,17,18], {31:11,18:12,19:13,15:14,7:15}, "GE LTA")
-master_copy = iterate(master_copy,leap_and_passport1, [1,10], [22, 4], { 7:0,18:8,16:10}, "Leap Passport 1")
-master_copy = iterate(master_copy, leap_and_passport2, [1,10],[19,4], {7:0, 18:7,16:9}, "Leap Passport 2")
-master_copy = iterate(master_copy,leap_and_passport3, [1,10], [1], {7:0, 18:7,16:9}, "Leap Passport 3")
-master_copy = iterate(master_copy,leap_tracker2, [1,10], [1,3], {4:2,31:8, 19:9, 7:30}, "Leap Tracker 2")
-master_copy = iterate(master_copy, leap_tracker3, [1,10], [0,1], {}, "Leap Tracker 3")
-master_copy = iterate(master_copy, mountain_top2, [1,10], [0], {7:1, 18:3,16:5, 14:7}, "Mountain Top 2")
-master_copy = iterate(master_copy, welded_ring2, [1,10], [1,2], {7:5, 16:6, 18:8, 19:9}, "Welded Ring 2")
-master_copy = iterate(master_copy,ge_lta2, [1,10],[1,2], {2:3, 18:7, 16:9, 19:8, 7:13, 15:5}, "GE LTA 2")
+#new stuff as of 10/18
+afsrr_leap_tracker= better_csv.get_lists(better_csv.get_lines(open("afsrr_leap_tracker.csv").read()))
+print "loaded afsrr"
+fontana= better_csv.get_lists(better_csv.get_lines(open("fontana.csv").read()))
+print "loaded fontana"
+frisa_leap_rings_tracker= better_csv.get_lists(better_csv.get_lines(open("frisa_leap_rings_tracker.csv").read()))
+print "loaded frisa leap"
+ge_leap_lta_tracker=better_csv.get_lists(better_csv.get_lines(open("ge_leap_lta_tracker.csv").read()))
+print "loaded ge lta leap"
+leap_program_updates=better_csv.get_lists(better_csv.get_lines(open("leap_program_updates.csv").read()))
+print "loaded leap program updates"
+leap_weekly_updates=better_csv.get_lists(better_csv.get_lines(open("leap_weekly_updates.csv").read()))
+print "loaded leap weekly updates"
+print "files loaded"
+master_copy = iterate(master_copy, afsrr_leap_tracker, [1,10], [1,3], {4:2,15:9,19:12,31:18,7:19}, "AFSRR Leap")
+master_copy = iterate(master_copy, fontana, [1,10],[1,3],{4:2,15:6,31:8,19:9,7:16}, "Fontana")
+master_copy = iterate(master_copy, frisa_leap_rings_tracker, [])
+# master_copy = iterate(master_copy, firth_rixon, [1,10],[16,17],{18:12, 16:15,19:13,31:11},"Firth Rixson")
+# master_copy = iterate(master_copy,welded_ring,[1,10],[1,2],{18:8,16:6,19:9,7:5}, "Welded Ring 1")
+# # master_copy = iterate(master_copy, raw_qpe,[1,10],[0],{4:6},"Raw QPE")
+# master_copy = iterate(master_copy,sandy, [1,10],[1,8],{7:6,11:9,14:12}, "Sandy's Data")
+# master_copy = iterate(master_copy,mountain_top, [1,10],[0], {18:3,16:5,7:1}, "Mountain Top")
+# master_copy = iterate(master_copy,cfw,[1,10],[0], {7:1, 18:2, 19:3, 15:4, 16:5, 14:7}, "CFW 1")
+# master_copy = iterate(master_copy,suzhou,[1,10],[0,6], {7:4,11:7,14:10}, "Suzhou 1")
+# master_copy = iterate(master_copy,leap_tracker,[1,10],[1,3], {4:2,31:8,19:9,7:30}, "LEAP tracker 1")
+# master_copy = iterate(master_copy, leap_tracker_welded_ring_update,[1,10],[1,2],{16:17,19:8,31:7,7:9,14:25}, "LEAP welded ring update")
+# master_copy = iterate(master_copy,tei,[1,10],[1,8],{7:6,11:9,14:12}, "TEI")
+# master_copy = iterate(master_copy,cfw_lta,[1,10],[0], {7:1,18:2,19:3,16:5,14:7}, "CFW LTA")
+# master_copy = iterate(master_copy, firth_rixson2, [1,10],[15,16,17,18], {31:10,18:11,19:12,15:13,7:14}, "Firth Rixson 2")
+# master_copy = iterate(master_copy,frisa_lta,[1,10],[2,3], {7:6,18:9,19:11}, "Frisa LTA")
+# master_copy = iterate(master_copy,ge_lta, [1,10],[16,17,18], {31:11,18:12,19:13,15:14,7:15}, "GE LTA")
+# master_copy = iterate(master_copy,leap_and_passport1, [1,10], [22, 4], { 7:0,18:8,16:10}, "Leap Passport 1")
+# master_copy = iterate(master_copy, leap_and_passport2, [1,10],[19,4], {7:0, 18:7,16:9}, "Leap Passport 2")
+# master_copy = iterate(master_copy,leap_and_passport3, [1,10], [1], {7:0, 18:7,16:9}, "Leap Passport 3")
+# master_copy = iterate(master_copy,leap_tracker2, [1,10], [1,3], {4:2,31:8, 19:9, 7:30}, "Leap Tracker 2")
+# master_copy = iterate(master_copy, leap_tracker3, [1,10], [0,1], {}, "Leap Tracker 3")
+# master_copy = iterate(master_copy, mountain_top2, [1,10], [0], {7:1, 18:3,16:5, 14:7}, "Mountain Top 2")
+# master_copy = iterate(master_copy, welded_ring2, [1,10], [1,2], {7:5, 16:6, 18:8, 19:9}, "Welded Ring 2")
+# master_copy = iterate(master_copy,ge_lta2, [1,10],[1,2], {2:3, 18:7, 16:9, 19:8, 7:13, 15:5}, "GE LTA 2")
+"""
+billet_diameter = line[15]
+yield_value = line[14]
+weight = line[16]
+weight_type = line[17]
+alloy = line[18]
+spec = line[19]
+qpe = line[4]
+raw_material_type = line[11]
+alloy_family = line[31]
+engine_program = line[7]
+engine_model = line[8]
+flash_welding_permitted = line[20]
+"""
+
 
 
 # print "Raw QPE: %s" % raw_qpe_count
